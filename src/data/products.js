67e69e5c9fec11bb3b0faf3b0e1f61e2dbfccaf0
su5867 <me.supriya07@gmail.com>
2026-05-19 17:20:51 +0545

    Issues

--- src/data/products.js
+++ src/data/products.js
@@ -20,8 +20,6 @@ export const VARIETIES = [
   { id: 'v4', name: 'Chips/Kurkure/Cheese Balls', description: 'Crunchy, flavourful, and irresistible.', image: '/images/products/Potato.jpg' },
   { id: 'v5', name: 'Puffs',                    description: 'Crunchy, flavourful, and irresistible.', image: '/images/products/Jungle_Janawar.png' },
   { id: 'v6', name: 'Diet & Health',            description: 'Crunchy, flavourful, and irresistible.', image: '/images/products/die_mixture.jpg' },
-  { id: 'v1', name: 'Boondi',            description: 'Crunchy, flavourful, and irresistible.', image: '/images/products/boondi_namkeen.jpg' },
-  { id: 'v2', name: 'Bhuja',            description: 'Crunchy, flavourful, and irresistible.', image: '/images/products/chatpate_bhujha_bag.jpeg' },
 ];
 
 export const PRODUCTS = [
